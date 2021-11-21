5a186a18479ffc933af84657e99c4f029bf35bca
Lauren Leslie <laurenlesliesilva@gmail.com>
2021-11-20 21:35:21 -0300

    Select a measure

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-function start(changedField){
-    const checkbox = document.getElementById('sameValue');
+const preview = document.getElementsByClassName('preview')[0];
+let measure = document.getElementById('measure').value;
 
-    if(checkbox.checked){
-        changeAllBorderRadius(changedField);
-    }
+function start(changedField) {
+    const checkbox = document.getElementById('sameValue');
+    const changedFieldValue = changedField.value
+
+    checkbox.checked === true ? changeAllBorderRadius(changedField) : changeBorderRadius(changedField.id, changedFieldValue);
+
 }
 
-function changeAllBorderRadius(changedField){
+function changeAllBorderRadius(changedField) {
     const newValue = changedField.value;
     const fields = [
         "topLeftRadius",
@@ -16,14 +19,38 @@ function changeAllBorderRadius(changedField){
     ];
 
     fields.forEach(field => {
-        const element = document.getElementById(field);
+        let element = document.getElementById(field);
         element.value = newValue;
     })
 
-    document.getElementsByClassName('preview')[0].style.borderRadius = newValue;
+    preview.style.borderRadius = newValue + measure;
 }
 
-// function changeBorderRadius(element, value) {
-//     element.style.borderRadius = value + 'px';
-// }
+function changeBorderRadius(element, value) {
+
+    switch (element) {
+        case "topLeftRadius":
+            preview.style.borderTopLeftRadius = value + measure;
+            break;
+        case "topRightRadius":
+            preview.style.borderTopRightRadius = value + measure;
+            break;
+        case "bottomLeftRadius":
+            preview.style.borderBottomLeftRadius = value + measure;
+            break;
+        case "bottomRightRadius":
+            preview.style.borderBottomRightRadius = value + measure;
+            break;
+        default: break;
+    }
+
+}
+
+function updateMeasure(newMeasure) {
+    measure = newMeasure;
+    changeBorderRadius('topLeftRadius', document.getElementById('topLeftRadius').value);
+    changeBorderRadius('topRightRadius', document.getElementById('topRightRadius').value);
+    changeBorderRadius('bottomLeftRadius', document.getElementById('bottomLeftRadius').value);
+    changeBorderRadius('bottomRightRadius', document.getElementById('bottomRightRadius').value);
+}
 
